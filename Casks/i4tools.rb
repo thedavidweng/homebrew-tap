89c4ec4c384c06ff035a524a15f21b7d61b51d7a
Davy <95214375+thedavidweng@users.noreply.github.com>
2026-06-14 14:51:38 -0700

    i4tools: use redirect URL for auto-update, skip checksum

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,11 +1,10 @@
 cask "i4tools" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.07.001"
-  sha256 arm:   "c0a4c52ce53bff79c5ffbd64c74c07313578d1430b9421edf84d5d8592a41c45",
-         intel: "443985cf6afb7b1e366b2d7e503ee3b769c8372074d1c07149c223bcb45373bd"
+  version :latest
+  sha256 :no_check
 
-  url "https://d-updater.i4.cn/i4tools9/download/macos/#{arch}/i4Tools_v#{version}_#{arch}.dmg",
+  url "https://url.i4.cn/FFRBr2aa",
       verified: "d-updater.i4.cn/"
   name "爱思助手"
   name "i4Tools"
