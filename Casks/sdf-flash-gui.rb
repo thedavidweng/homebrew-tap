cd7fb2ad97883a7056722450b892af287659b220
Davy <95214375+thedavidweng@users.noreply.github.com>
2026-06-26 16:24:25 -0700

    fix: point sdf-flash-gui 0.2.0 cask at rebuilt SDF.Flash.GUI DMGs

--- Casks/sdf-flash-gui.rb
+++ Casks/sdf-flash-gui.rb
@@ -2,8 +2,8 @@ cask "sdf-flash-gui" do
   version "0.2.0"
 
   on_arm do
-    sha256 "71045ee4c9ad3c992c922c028b53f9c126454da54bdeb7a6bc811b67de4d9376"
-    url "https://github.com/thedavidweng/sdf-flash-gui/releases/download/v0.2.0/SDFFlashGUI_0.2.0_aarch64.dmg"
+    sha256 "22372ba55b329a544d1f7f59fe6b7bb217bfb47c34f3e3b11592c90be14375dc"
+    url "https://github.com/thedavidweng/sdf-flash-gui/releases/download/v0.2.0/SDF.Flash.GUI_0.2.0_aarch64.dmg"
   end
   on_intel do
     sha256 "4440546e5e75079f1a4143519665cf8788ff63c0adc692ef751680ba77880d51"
